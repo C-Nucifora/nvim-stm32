19247d6fc8fa4027f5937e1f1ce124db8d55df11
C-Nucifora <143251500+C-Nucifora@users.noreply.github.com>
2026-09-04 17:10:21 +1000

    chore(fixtures): normalize copied fixture whitespace

--- tests/fixtures/startup_only/startup_stm32f429xx.s
+++ tests/fixtures/startup_only/startup_stm32f429xx.s
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file      startup_stm32f429xx.s
   * @author    MCD Application Team
-  * @brief     STM32F429xx Devices vector table for GCC based toolchains. 
+  * @brief     STM32F429xx Devices vector table for GCC based toolchains.
   *            This module performs:
   *                - Set the initial SP
   *                - Set the initial PC == Reset_Handler,
